064c4e59bf1c844feca3429ec557a00749398240
simon <simonbw@uw.edu>
2017-08-31 17:04:15 -0700

    Small css/text changes

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -15,8 +15,9 @@ export default class Credits extends React.Component<any, { [open: string]: bool
         return (
             <div className={styles.Credits} onClick={() => this.toggleOpen()}>
                 {this.state.open ?
-                    <p>The impulse response used for the reverb is licensed under the
-                        <a href="https://creativecommons.org/licenses/by-sa/3.0/legalcode">CC-SA 3.0</a>
+                    <p>
+                        The impulse response used for the reverb is licensed under the
+                        {' '}<a href="https://creativecommons.org/licenses/by-sa/3.0/legalcode">CC-SA 3.0</a>{' '}
                         and created by: <br/>
                         <a href="http://www.openairlib.net/auralizationdb/content/lady-chapel-st-albans-cathedral">www.openairlib.net</a><br/>
                         Audiolab, University of York<br/>
